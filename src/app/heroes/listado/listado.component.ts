f37ba6ce8b524ed6626d992b2f036836aac7a8a7
JorgeVargas <javargas216@unicauca.edu.co>
2022-07-12 11:43:45 -0500

    First Commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Capitán America',
+  ];
+
+  borrado: string = '';
+
+  borrarHeroe() {
+    this.borrado = this.heroes.shift() || "";  
+  }
+}
